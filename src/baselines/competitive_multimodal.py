63943c7525907c3fb8b06a400b7060f3f98e18b6
Atharva Naik <atharvanaik2018@gmail.com>
2023-03-28 22:19:38 -0400

    updating ViLT experiment stats and adding CLIP zero shot recall chart

---
+++ src/baselines/competitive_multimodal.py
@@ -0,0 +1,77 @@
+# competitive multimodal baselines.
+import os
+import json
+import torch
+from PIL import Image
+from tqdm import tqdm
+import torch.nn as nn
+import matplotlib.pyplot as plt
+from torch.utils.data import DataLoader
+from src.datautils import VizWizVQABestAnsDataset
+
+def clip_score_recall_analysis(cos_scores_path: str, 
+                               data_path: str="./data/VQA", 
+                               split: str="val"):
+    dataset = VizWizVQABestAnsDataset(
+        os.path.join(data_path, f"{split}.json"),
+        os.path.join(data_path, split),
+    )   
+    all_classes = json.load(open("experiments/vizwiz_class_vocab.json"))
+    true_indices = [all_classes[ans["answer"]] for ans in dataset] 
+    cos_scores = torch.load(cos_scores_path, map_location="cpu")
+    recall_at_k = {}
+    ks = [1,5,10,15,20,25,30,35,40,45,50,100,200,300,400,500,1000,2000,3000,4000,5000]
+    for k in ks:
+        topk_inds = torch.topk(cos_scores, k=k).indices.tolist()
+        recall_at_k[k] = 0
+        for i in range(len(true_indices)):
+            if true_indices[i] in topk_inds[i]:
+                recall_at_k[k] += 1
+        recall_at_k[k] /= len(true_indices)
+    plt.clf()
+    # print(recall_at_k)
+    y = list(recall_at_k.values())
+    x = range(len(ks))
+    plt.plot(x, y)
+    plt.xlabel("k")
+    plt.ylabel("recall@k")
+    plt.title("CLIP score recalls vs k")
+    plt.xticks(x, labels=ks, rotation=45)
+    plt.tight_layout()
+    plt.savefig("./experiments/clip_cos_scores_vizwiz_recall.png")
+
+def test_CLIP_zero_shot():
+    """Test CLIP L-14 in a zero shot way for this task
+    This version ignores the question text"""
+    from sentence_transformers import SentenceTransformer, util
+
+    # Load CLIP model
+    model = SentenceTransformer('clip-ViT-L-14')
+    # technically not needed but I'll do it anyways
+    if torch.cuda.is_available(): model.cuda()
+    dataset = VizWizVQABestAnsDataset(
+        "./data/VQA/val.json", 
+        "./data/VQA/val",
+    )
+    dataloader = DataLoader(dataset)
+    all_classes = json.load(open("experiments/vizwiz_class_vocab.json"))
+    true_indices = []
+    all_answer_texts = list(all_classes.keys())
+    ans_emb = model.encode(all_answer_texts, show_progress_bar=True, convert_to_tensor=True)
+    # Encode all images:
+    img_emb = []
+    for batch in tqdm(dataset):
+        true_indices += [all_classes[ans] for ans in batch["answer"]]
+        img_emb.append(model.encode([Image.open(img) for img in batch["image"]], convert_to_tensor=True))
+    img_emb = torch.stack(img_emb)
+    # compute cosine similarities 
+    cos_scores = util.cos_sim(img_emb, ans_emb)
+    # print(cos_scores)
+    return cos_scores, true_indices
+
+class CLIPEnsemble(nn.Module):
+    """CLIP Ensemble model based on this
+    arxiv paper: https://arxiv.org/pdf/2206.05281.pdf"""
+    def __init__(self, num_classes: int, answer_types: int):
+        super().__init__()
+        self.num_classes = num_classes
