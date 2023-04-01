b94efc36d74fecab74d24cccff16ad72f2c1858b
Yash Butala <butalayash99@gmail.com>
2023-04-01 00:57:42 -0400

    Script for eval from local

---
+++ src/eval.py
@@ -0,0 +1,36 @@
+from src.PythonHelperTools.vqaTools.vqa import VQA
+from src.PythonEvaluationTools.vqaEvaluation.vqaEval import VQAEval
+import json
+
+def get_eval_scores(annFile, resFile):
+    gold_json = json.load(open(annFile))
+    pred_json = json.load(open(resFile))
+    formatted_pred_json = []
+    for gold_exp, pred_exp in zip(gold_json,pred_json["preds"]):
+        formatted_exp = {"image": gold_exp["image"], "answer": pred_exp["pred"]}
+        formatted_pred_json.append(formatted_exp)
+    formatted_file = f"experiments/clip/formatted_pred.json"
+    f = json.dump(formatted_pred_json, open(formatted_file, "w"), indent=4)
+    vqa = VQA(annFile)
+    vqaRes = VQA(formatted_file)
+    vqaEval = VQAEval(vqa, vqaRes, n=2)
+    # evaluate VQA results
+    vqaEval.evaluate()
+    # print accuracies
+    print("\n")
+    print("Overall Accuracy is: %.02f\n" %(vqaEval.accuracy['overall']))
+    print("\n")
+    print("Per Answer Type Accuracy is the following:")
+    for ansType in vqaEval.accuracy['perAnswerType']:
+        print("%s : %.02f" %(ansType, vqaEval.accuracy['perAnswerType'][ansType]))
+    print("\n")
+    print("Caption metrics are :")
+    for k, v in list(vqaEval.caption_metric.items()):
+        print("%s: %.2f"%(k,v))
+
+    
+if __name__ == "__main__":
+
+    
+    get_eval_scores("data/VQA/val.json", 
+                f'experiments/clip/pred.json')
