21eccc084c7c4cdd1e280fed1a1e94f164a8d845
Atharva Naik <atharvanaik2018@gmail.com>
2023-05-05 14:02:49 -0400

    qual examples for error anal of vilt skill clf

--- scripts/sample_egs_for_qual_analysis.py
+++ scripts/sample_egs_for_qual_analysis.py
@@ -3,12 +3,17 @@
 # sample exaples for qual analysis from ViLT skill classifier:
 import os
 import json
+import random
+import pandas as pd
 from collections import defaultdict
 
-vilt_skill_clf2_pred = json.load(open("./experiments/vilt_skill_clf2/predict_logs.json"))["preds"]
+random.seed(42)
+
+vilt_skill_clf_pred = json.load(open("./experiments/vilt_skill_clf/predict_logs.json"))["preds"]
+skill_data = pd.read_csv("./data/skill/vizwiz_skill_typ_val.csv").to_dict("records")
 ctr = 0
 skill_label_wise = defaultdict(lambda: {"false_pos": [], "false_neg": []})
-for i, rec in enumerate(vilt_skill_clf2_pred):
+for i, rec in enumerate(vilt_skill_clf_pred):
     for label in rec["true"]:
         if label not in rec["pred"]:
             skill_label_wise[label]["false_neg"].append(i)
@@ -16,6 +21,25 @@ for i, rec in enumerate(vilt_skill_clf2_pred):
         if label not in rec["true"]:
             skill_label_wise[label]['false_pos'].append(i)
 skill_label_wise = dict(skill_label_wise)
+df = []
 for skill in skill_label_wise:
     print(f"{skill}:")
-    print("FP:", len(skill_label_wise[skill]["false_pos"]), "FN:", len(skill_label_wise[skill]["false_neg"]))
+    FPs = skill_label_wise[skill]["false_pos"]
+    nFP = len(FPs)
+    FNs = skill_label_wise[skill]["false_neg"]
+    nFN = len(FNs)
+    print("FP:", nFP, "FN:", nFN)
+    for i in random.sample(FPs, min(5, nFP)):
+        df.append({
+            "image": skill_data[i]["IMG"],
+            "question": skill_data[i]["QSN"],
+            "error_type": skill + " False Positive",
+        })
+    for id in random.sample(FNs, min(5, nFN)):
+        df.append({
+            "image": skill_data[i]["IMG"],
+            "question": skill_data[i]["QSN"],
+            "error_type": skill + " False Negative",
+        })
+df = pd.DataFrame(df)
+df.to_csv("./vilt_skill_clf_error_anal.csv", index=False)
